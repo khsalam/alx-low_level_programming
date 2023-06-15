f65fd124442fe216923fc165842818f512a11f79
= <=>
2023-06-15 14:30:31 -0700

    fix 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main: Main function to print first line
- * Print Programming is like building a multilingual puzzle
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {

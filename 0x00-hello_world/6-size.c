b29c64e8a84d5c732ed8cc534668c4beaadee8da
= <=>
2023-06-15 11:24:47 -0700

    end 00

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * Print Programming is like building a multilingual puzzle
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}

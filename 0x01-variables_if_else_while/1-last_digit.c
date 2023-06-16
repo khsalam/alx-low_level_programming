bdab65d926019efd03df4f88ea9f6134b1ba6911
= <=>
2023-06-16 08:26:15 -0700

    Check reminder

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the sign of the variable
+ * betty style doc for function main goes there
+ * Return: always return 0
+ */
+int main(void)
+{
+	int n;
+        int is;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	is = n % 10;
+	if (is > 5 )
+		printf("Last digit of %d is %d and is greater than 5\n", n,is);
+	if (is == 0)
+		printf("Last digit of %d is %d and is zero\n", n,is);
+	if (is < 6 & is != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
+	return (0);
+}

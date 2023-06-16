3f22396efb6c9678417c9820045adb50804dc3a4
= <=>
2023-06-16 08:04:25 -0700

    Positive or negaative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - print the sign of the variable
+ * betty style doc for function main goes there 
+ * Return: always return 0
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0) 
+	if (n > 0) 
 		printf("%d\n is Positive", n);
 	if( n ==0 )
 		printf("%d\n is zero", n);

a2ad6d20c6784473a1093b28a7849794c8bf603d
= <=>
2023-06-16 08:00:18 -0700

    Positive or negaative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0) 
-		printf("%d\n", " is Positive);
+		printf("%d\n is Positive", n);
 	if( n ==0 )
-		printf("%d\n", " is zero");
+		printf("%d\n is zero", n);
 	if (n < 0 ) 
-		printf("%d\n", " is negative");
+		printf("%d\n is negative", n);
 	return (0);
 }

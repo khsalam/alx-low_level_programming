1bcea8e9f15cb01ebb80fb62d1cba6cc2ee9bb1a
= <=>
2023-06-16 12:54:57 -0700

    Fix Mandatory

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,10 +11,10 @@ int main(void)
 	char c1;
 	int n;
 
-	for(n = 48; n <=57; n++)
+	for(n = 48; n <= 57; n++)
 		putchar(n);
 
-	for (c = 'a'; c <='f'; c++)
+	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}

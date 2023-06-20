112d6a14b4d9201d9a52a145a2f00c6094d8186b
= <=>
2023-06-20 12:53:38 -0700

    Check iflower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,6 +8,7 @@ void print_alphabet_x10(void)
 	char c[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 	int j;
+
 	for (j = 0; j < 10; j++)
 		{
 		for (i = 0; i < 26; i++)
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+@include "ctype.h"
+
+/**
+ * _islower - check if lower case chart
+ */
+int _islower(int c)
+{
+	if (islower(c) >0)
+		return(1);
+	else
+		return(0);
+			
+}

2eea66b7d253fc1b28c642be059dcc1b0f4939e8
= <=>
2023-06-20 08:45:14 -0700

    fu

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * main - print the _put char					
- * betty style doc for function main goes there
- * Return: always return 0
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
  */
 int main(void)
 {
 	char c[] = "_putchar";
 	int i;
-	for (i = 0 ; i < 8; i++)
+
+	for (i = 0; i < 9; i++)
+	{
 		_putchar(c[i]);
-	_putchar ('\n');
+	}
+	_putchar('\n');
 	return (0);
 }

7d2bb3050736018d6eaeb089249a896f4930e61e
= <=>
2023-06-20 13:58:27 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "ctype.h"
+
+/**
+ *_abs(int) - check if input is alpha
+ *@c: the Input paramater
+ *
+ * Return: 1 for positive -1 for negative else 0
+ */
+int _abs(int c);
+{
+	int r;
+
+	r = abs(c);
+	return (r);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "ctype.h"
+
+/**
+ *print_last_digit - check if input is alpha
+ *@c: the Input paramater
+ *
+ * Return: last digit
+ */
+int print_last_digit(int c)
+{
+	int r;
+
+	r = c % 10;
+	return (r);
+}

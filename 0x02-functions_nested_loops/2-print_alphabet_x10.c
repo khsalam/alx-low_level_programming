04c908d323cd760c7e07c328c00e6d81b2067389
= <=>
2023-06-20 12:40:54 -0700

    alpha 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the lowe alaphabet
+ * print_alphabet_x10 - prints the lowe alaphabet
  */
 void print_alphabet_x10(void)
 {
@@ -9,10 +9,11 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 	for (j = 0; j < 10; j++)
-
+		{
 		for (i = 0; i < 26; i++)
 		{
 			_putchar(c[i]);
-		}	
-	_putchar('\n');
+		}
+		_putchar('\n');
+		}
 }

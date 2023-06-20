e06838c26241746d1936dbd6c1eaf6d57d86ed3d
= <=>
2023-06-20 07:34:24 -0700

    include main.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,1 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
-/**
- * main - print the _put char					
- * betty style doc for function main goes there
- * Return: always return 0
- */
-int main(void)
-{
-	printf("_putchar");
-	return (0);
-}
+#include <main.c>

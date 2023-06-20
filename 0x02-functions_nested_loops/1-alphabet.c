bdebf78eaa3024d5f55a37980fb0e233ce20d876
= <=>
2023-06-20 12:28:00 -0700

    Print alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - prints the string "_putchar" from a character array.
- * Return: 0 on success.
+ * print_alphabet - prints the lowe alaphabet
  */
-int main(void)
+void print_alphabet(void)
 {
 	char c[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
@@ -14,5 +13,4 @@ int main(void)
 		_putchar(c[i]);
 	}
 	_putchar('\n');
-	return (0);
 }

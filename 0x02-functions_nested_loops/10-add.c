5c8fb0c6e902c20884f693620af865a70a36a6d8
= <=>
2023-06-21 15:22:54 -0700

    add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "ctype.h"
+
+/**
+ *times_table - print time table
+ *@a - First
+ *@b - Second
+ @Return: Return sum
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}

474762e7456297c129ed34a2478c0298e40c210e
= <=>
2023-06-21 15:48:55 -0700

    printf function

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,14 @@ void print_to_98(int n)
 
 	if ( n < 98 )
 	{
-		for ( i = n ; n <= 98 ;i++)
+		for ( i = n ; i <= 98 ;i++)
 			printf("%d, ",i);
 	}
+	else if ( n == 98)
+		printf("%d",98);
 	else
 	{
-		for (i = n ; n >= 98 ; i--)
+		for (i = n ; i >= 98 ; i--)
 			printf("%d, ",i);
 	}
 }

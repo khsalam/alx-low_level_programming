56ca6e01109bad14fabc088be34358deb60e7b3d
= <=>
2023-06-21 15:34:13 -0700

    printf function

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "ctype.h"
+#include "stdlib"
+
+/**
+ *print_to_98 - return sum
+ *@n: - First
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if ( n < 98 )
+	{
+		for ( i = n ; n <= 98 ; n++)
+			printf("%d, ",n);
+	}
+	else
+	{
+		for (i = n ; n >= 98 ; n--)
+			printf("%d, ",n);
+	}
+}

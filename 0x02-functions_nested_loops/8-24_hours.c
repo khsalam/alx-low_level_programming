21c47137aef9200e56c856a310fec76045129aa0
= <=>
2023-06-21 14:31:43 -0700

    print min

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,48 @@
+#include "main.h"jack_bauer
+#include "ctype.h"
+
+/**
+ *jack_bauer - print every min  
+ *@c: the Input paramater
+ *
+ * Return: last digit
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+	int f;
+	int l;
+
+	for (h = 0; h <= 23; h++)
+		for (m = 0; m <= 59; m++)
+		{
+			f = h % 10;
+			if (h < 10)
+			{
+				_putchar('0');
+				_putchar(h + '0');
+			}
+			else
+			{
+				f = h % 10;
+				l = h / 10;
+				_putchar(f + '0');
+				_putchar(l + '0');
+			}
+			_putchar(':');
+			if (m < 10)
+			{
+				_putchar('0');
+				_putchar(m + '0');
+			}
+			else
+			{
+				f = m % 10;
+				l = m / 10;
+				_putchar(f + '0');
+				_putchar(l + '0');
+			}
+			_putchar('\n');
+		}
+}

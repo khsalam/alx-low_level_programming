69d3d7f32e0aaacac37659e462e324581855d904
= <=>
2023-06-22 13:44:05 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - checks for uppercase.
+ * @a: First int .
+ * @b: Second int
+ * Return: multible of a* b.
+ */
+oid print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		_putchar(c + '0');
+
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 int _putchar(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);

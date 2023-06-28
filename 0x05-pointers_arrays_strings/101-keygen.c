b04b7b98909c1509d09196682c56c70b88b7ca16
= <=>
2023-06-28 01:40:16 -0700

    Random Password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "time.h"
 
 /**
  * main - program that generates random valid Password
@@ -8,23 +9,15 @@
  */
 int main(void)
 {
-	int pass[100];
-	int i, sum, n;
+	char pass[14];
+	int i;
 
-	sum = 0;	
+	srand(time(0));
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 14; i++)
 	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
+		pass[i] = (rand() % (126 - 32)) + '!';
+		putchar(pass[i]);
 	}
 
 	return (0);
